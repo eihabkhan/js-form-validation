e85dd549b0b6ce9d16c1be2ec92ffa8b67f5f7c5
Eihab <mastereihab@gmail.com>
2020-07-20 13:10:43 +0100

    Refactor code for validation

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const confPassword = document.getElementById("confPassword");
+const confPassword = document.getElementById("password confirmation");
 
-// Functions
+// FUNCTIONS
 function showFail(input, message) {
     // Displays an error message to the user based on the given parameter
     const formControl = input.parentElement;
@@ -19,44 +19,55 @@ function showSuccess(input) {
     formControl.classList.add("success");
 }
 
-function isValidEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (re.test(String(input.value.trim()).toLowerCase())) {
+        showSuccess(input);
+    } else {
+        showFail(input, "Email is not valid");
+    }
 }
 
-// Event Listeners
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function checkRequired(inputs) {
+    inputs.forEach(function(input){
+        if (input.value.trim() === ""){
+            showFail(input, `${getFieldName(input)} is required`)
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showFail(input, `${getFieldName(input)} should be at least ${min} characters`);
+    } else if(input.value.length > min) {
+        showFail(input, `${getFieldName(input)} should be no more than ${max} characters long`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+function checkPasswordMatch(inputA,inputB) {
+    if(inputA.value !== inputB.value) {
+        showFail(inputB, "Passwords don't match")
+    } else {
+        showSuccess(inputA)
+        showSuccess(inputB)
+    }
+}
+
+
+// EVENT LISTENERS
 form.addEventListener("submit", function(e) {
     e.preventDefault();
-    if (username.value === "") {
-        //TODO:
-        showFail(username, "Username is requried")
-    } else {
-        showSuccess(username);
-    }
-    
-    if (email.value === "") {
-        //TODO:
-        showFail(email, "Email is requried")
-    } else if (!isValidEmail(email.value)) {
-        showFail(email, "Please enter a valid email")
-    }
-    else {
-        showSuccess(email);
-    }
-
-    if (password.value === "") {
-        //TODO:
-        showFail(password, "Password is requried")
-    } else {
-        showSuccess(password);
-    }
-
-    console.log(confPassword)
-
-    if (confPassword.value === "") {
-        //TODO:
-        showFail(confPassword, "Password does not match")
-    } else {
-        showSuccess(confPassword);
-    }
+    checkRequired([username, email, password, confPassword]);
+    checkLength(username, 3, 15);
+    checkLength(password, 8, 25);
+    checkEmail(email)
+    checkPasswordMatch(password, confPassword)
 });
